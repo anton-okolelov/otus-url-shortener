8b7343bf8d3abafc26d29ef1cad5afa93d57b171
Anton Okolelov <anton.okolelov@binomo.com>
2019-06-11 15:05:26 +0300

    Fix code review

--- shortener.go
+++ shortener.go
@@ -7,33 +7,30 @@ import (
 	"sync/atomic"
 )
 
-var urls sync.Map
-var counter uint32
+type UrlShortener struct {
+	urls    sync.Map
+	counter uint32
+}
 
-func Shorten(url string) string {
-	position := atomic.AddUint32(&counter, 1)
+func (shortener *UrlShortener) Shorten(url string) string {
+	position := atomic.AddUint32(&(shortener.counter), 1)
 	shortPath := fmt.Sprintf("/%x", position)
 
-	urls.Store(shortPath, url)
+	shortener.urls.Store(shortPath, url)
 
 	return fmt.Sprintf("https://%s%s", "domain.com", shortPath)
 }
 
-func Resolve(url2resolve string) string {
+func (shortener *UrlShortener) Resolve(url2resolve string) string {
 	components, err := url.Parse(url2resolve)
 	if err != nil {
 		return ""
 	}
 
-	resolvedUrl, ok := urls.Load(components.Path)
+	resolvedUrl, ok := shortener.urls.Load(components.Path)
 	if ok {
 		return resolvedUrl.(string)
 	} else {
 		return ""
 	}
-}
-
-func clear() {
-	urls = sync.Map{}
-	counter = 0
 }
--- shortener_test.go
+++ shortener_test.go
@@ -6,18 +6,16 @@ import (
 )
 
 func TestShorten(t *testing.T) {
-	clear()
+	shortener := UrlShortener{}
+	assert.Equal(t, "https://domain.com/1", shortener.Shorten("http://test.ru/asdfasdf"))
+	assert.Equal(t, "https://domain.com/2", shortener.Shorten("http://test.ru/fasdf"))
 
-	assert.Equal(t, "https://domain.com/1", Shorten("http://test.ru/asdfasdf"))
-	assert.Equal(t, "https://domain.com/2", Shorten("http://test.ru/fasdf"))
-
-	assert.Equal(t, "http://test.ru/asdfasdf", Resolve("https://domain.com/1"))
-	assert.Equal(t, "http://test.ru/fasdf", Resolve("https://domain.com/2"))
+	assert.Equal(t, "http://test.ru/asdfasdf", shortener.Resolve("https://domain.com/1"))
+	assert.Equal(t, "http://test.ru/fasdf", shortener.Resolve("https://domain.com/2"))
 }
 
 func TestNonExistent(t *testing.T) {
-	clear()
-
-	assert.Equal(t, "", Resolve("invalid url"))
-	assert.Equal(t, "", Resolve("https://valid.ru/non-existent"))
+	shortener := UrlShortener{}
+	assert.Equal(t, "", shortener.Resolve("invalid url"))
+	assert.Equal(t, "", shortener.Resolve("https://valid.ru/non-existent"))
 }
